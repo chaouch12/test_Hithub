bd772b226f35fe263957a54ae0e76461b048dfbc
chaouch12 <63297109+chaouch12@users.noreply.github.com>
2020-04-07 15:14:44 +0200

    dwdewd

--- test_Hithub/Form1.cs
+++ test_Hithub/Form1.cs
@@ -16,5 +16,10 @@ namespace test_Hithub
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //halo
+        }
     }
 }
